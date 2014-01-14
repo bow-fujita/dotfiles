cde9273f65642ee7cfeab1578a4016d44cd53b0f
Hiromitsu FUJITA <bow.fujita@gmail.com>
2014-01-13 23:24:58 -0800

    Update rvm.sh.

--- bashrc.d/rvm.sh
+++ bashrc.d/rvm.sh
@@ -1,7 +1,15 @@
-if ! which rvm >& /dev/null; then
+if ! rvm -v >& /dev/null; then
 	if [ -f /etc/profile.d/rvm.sh ]; then
 		. /etc/profile.d/rvm.sh
 	elif [ -f ~/.rvm/scripts/rvm ]; then
 		. ~/.rvm/scripts/rvm
 	fi
+fi
+
+if rvm -v >& /dev/null; then
+	rvm_default="`cat ~/.rvm-default 2> /dev/null`"
+	if [ -n "$rvm_default" ]; then
+		rvm --default use $rvm_default
+	fi
+	unset rvm_default
 fi
