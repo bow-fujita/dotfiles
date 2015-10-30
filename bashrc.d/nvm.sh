7856c75b0606fb4aa0b305e1ab13f006c011602a
Hiromitsu FUJITA <bow.fujita@gmail.com>
2015-10-31 08:26:26 +0900

    Added procedures to load the system-wide NVM.

--- bashrc.d/nvm.sh
+++ bashrc.d/nvm.sh
@@ -1,15 +1,21 @@
 if ! nvm ls >& /dev/null; then
-	if [ -f ~/.nvm/nvm.sh ]; then
-		. ~/.nvm/nvm.sh
-	fi
+	for nvm_dir in ~/.nvm /usr/local/nvm; do
+		if [ -f $nvm_dir/nvm.sh ]; then
+			. $nvm_dir/nvm.sh
+			break
+		fi
+	done
+	unset nvm_dir
 fi
 
 if nvm ls >& /dev/null; then
-	nvm_default="`cat ~/.nvm-default 2> /dev/null`"
-	if [ -n "$nvm_default" ]; then
-		nvm alias default $nvm_default
-	fi
-	unset nvm_default
+	for nvm_default in ~/.nvm-default /etc/default/nvm-default; do
+		node_version="$(cat $nvm_default 2> /dev/null)"
+		if [ -n "$node_version" ]; then
+			nvm use $node_version
+		fi
+	done
+	unset nvm_default node_version
 fi
 
 if [ -f ~/.npm_completion ]; then
