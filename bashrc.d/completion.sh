2978cfbe4d6a507e3c5c83a253beb9d8bcc3d57b
Hiro Fujita <bow.fujita@gmail.com>
2022-06-23 11:26:03 -0700

    Added bash-completion from homebrew

--- bashrc.d/completion.sh
+++ bashrc.d/completion.sh
@@ -4,4 +4,9 @@ if ! shopt -oq posix; then
   elif [ -f /etc/bash_completion ]; then
     . /etc/bash_completion
   fi
+  if which brew > /dev/null; then
+    if [ -r $(brew --prefix)/etc/profile.d/bash_completion.sh ]; then
+      . $(brew --prefix)/etc/profile.d/bash_completion.sh
+    fi
+  fi
 fi
